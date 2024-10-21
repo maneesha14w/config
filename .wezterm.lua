a5870ebd26556ee16c6abed54de50d3720576a2d
Maneesha Walisundera <maneesha14w@gmail.com>
2024-10-21 12:57:20 +0530

    chore: theme change

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- Theming
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'rose-pine'
 config.font = wezterm.font('0xProto Nerd Font')
 config.font_size = 14
 
